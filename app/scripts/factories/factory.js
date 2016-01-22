85da2d300d6fa2d4c0eaa033405abc0230430935
spaetsch <spaetsch@gmail.com>
2016-01-21 16:24:55 -0800

    dependencies resolved, but pics not loading.

--- app/scripts/factories/factory.js
+++ app/scripts/factories/factory.js
@@ -5,15 +5,10 @@ console.log('top of factory.js');
 var app = angular.module('intFlickApp');
 angular.module('myFactory',[]);
 
-app.factory('myFactory', function($http, $q){
+app.factory('myFactory', function($http){
   var service = {};
-
-  service.callFlickr = function(){
-    console.log('first line service callFlickr');
-    var deferred = $q.defer();
-
-      return deferred.promise;
-  };
+  var APIkey = '&api_key=fa3e0832f30851339c73d3dd3c27f961';
+  var testURL = 'https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=7cd4e1a19694735a54aa19dd860e5855&date=2016-01-01&per_page=25&page=1&format=json&nojsoncallback=1'+ APIkey;
 
   service.buildURL = function(data, next){
 
@@ -31,6 +26,28 @@ app.factory('myFactory', function($http, $q){
 
     return photoURL;
   };
+
+  service.callFlickr = function(){
+    console.log('first line service callFlickr');
+      //GET call using promises instead of callbacks
+    $http({ method: 'GET', url: testURL})
+      //success
+      .then(function(response) {
+        //console.log(response.data);
+        this.pics = response.data.photos.photo;
+
+        //console.log('$scope.pics', $scope.pics);
+        //adds image URL to array
+        for (var i=0; i < response.data.photos.photo.length; i++){
+          this.pics[i].picURL = this.buildPhotoLink(response.data, i);
+        }
+      },
+      //error
+      function() {
+        console.log('error');
+      });
+  };
+
 
   return service;
 });
