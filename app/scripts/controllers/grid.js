e0b34019d5e97dd01f3686755077a0b172bbf5a7
spaetsch <spaetsch@gmail.com>
2016-01-22 19:38:02 -0800

    better url

--- app/scripts/controllers/grid.js
+++ app/scripts/controllers/grid.js
@@ -8,8 +8,10 @@
  * Controller of the intFlickApp
  */
 
-var APIkey = '&api_key=fa3e0832f30851339c73d3dd3c27f961';
-var testURL = 'https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=7cd4e1a19694735a54aa19dd860e5855&date=2016-01-01&per_page=24&page=1&format=json&nojsoncallback=1'+ APIkey;
+var baseURL = 'https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=c122c5b37d7f44cecd205b35066fd584';
+var searchDate = '&date=2016-01-01';
+var format = '&extras=owner_name%2C+url_z&per_page=24&format=json&nojsoncallback=1';
+var requestURL = baseURL + searchDate + format;
 
 angular.module('intFlickApp')
   .controller('GridCtrl', function ($scope, $http, myFactory) {
@@ -19,7 +21,7 @@ angular.module('intFlickApp')
       'Karma'
     ];
     //GET call using promises instead of callbacks
-    $http({ method: 'GET', url: testURL})
+    $http({ method: 'GET', url: requestURL})
       //success
       .then(function(response) {
         $scope.pics = myFactory.successFlickr(response);
@@ -33,7 +35,6 @@ angular.module('intFlickApp')
     $scope.pics = myFactory.pics;
 
     $scope.$watch('myFactory.pics', function (newVal, oldVal, scope) {
-
       if (newVal) {
         scope.pics = newVal;
       }
--- app/scripts/factories/factory.js
+++ app/scripts/factories/factory.js
@@ -8,29 +8,21 @@ app.factory('myFactory', function(){
     pics:[]
   };
 
-  service.buildPicURL = function(data, next){
-    var farm = data.photos.photo[next].farm;
-    var server = data.photos.photo[next].server;
-    var id = data.photos.photo[next].id;
-    var secret = data.photos.photo[next].secret;
+  service.buildFlickrURL = function(data, next){
+    var baseURL = 'https://www.flickr.com/photos/';
+    var userID = data.photos.photo[next].owner;
+    var photoID = data.photos.photo[next].id;
 
-    //assemble the parts into a complete URL for the photo
-    var photoURL = 'https://farm' + farm + '.staticflickr.com/' + server + '/' + id + '_' + secret + '_z.jpg';  //underscore letter signals size of resultb
-    // z medium 640, 640 on longest side
-    // c medium 800, 800 on longest side
-    // b large, 1024 on longest side
-    // h large 1600, 1600 on longest side
-    return photoURL;
+    return baseURL + userID + '/' + photoID;
   };
 
   service.successFlickr = function(response){
     this.pics = response.data.photos.photo;
-    //adds image URL to array
+    //adds flickr URL to array
     for (var i=0; i < response.data.photos.photo.length; i++){
-      this.pics[i].picURL = this.buildPicURL(response.data, i);
+      this.pics[i].flickrURL = this.buildFlickrURL(response.data, i);
     }
     console.log('in service successFlickr, after for loop, returning this.pics=', this.pics);
-
     return this.pics;
   };
   return service;
